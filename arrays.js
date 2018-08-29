39ea82f30b992c28fa37891f0ae994688921ee04
yoan ante <yoanante@gmail.com>
2018-08-29 19:44:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,11 @@
 var chocolateBars = new Array("snickers", "hundred grand", "kitkat", "skittles");
+
+addElementToBegginingOfArray(ary, element){
+  return [element,...ary]
+}
+
+destructivelyAddElementToBegginingOfArray(){
+  
+}
+
+console.log(addElementToBegginingOfArray(chocolateBars, "almond joy"))
